eff8687110980c323710fdf3fddaf7b1b6264ba7
Cherpakov-Kirill <cherpakov-kirill-42@mail.ru>
2022-10-31 21:41:54 +0700

    Deleted semicolon

--- PickyBrideTests/ContenderGeneratorTests.cs
+++ PickyBrideTests/ContenderGeneratorTests.cs
@@ -28,7 +28,6 @@ public class ContenderGeneratorTests
             .Should()
             .Throw<ApplicationException>()
             .WithMessage(PickyBride.resources.NumberOfContendersShouldBeMoreThenZero);
-        ;
     }
 
     [Test]
